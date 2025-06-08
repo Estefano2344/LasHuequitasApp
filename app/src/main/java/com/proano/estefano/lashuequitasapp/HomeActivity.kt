81a01ad328185eeb616fc33aec635a9d580d7176
DavidQuille <david.quille@epn.edu.ec>
2025-06-07 23:30:41 -0500

    create barra lateral

--- app/src/main/java/com/proano/estefano/lashuequitasapp/HomeActivity.kt
+++ app/src/main/java/com/proano/estefano/lashuequitasapp/HomeActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.core.view.GravityCompat
+import android.widget.ImageView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,12 @@ class HomeActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        val opciones = findViewById<ImageView>(R.id.opcionesImageView)
+        opciones.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
         }
     }
 }
