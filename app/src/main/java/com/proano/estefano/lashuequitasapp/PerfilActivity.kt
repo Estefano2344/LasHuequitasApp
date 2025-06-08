df5332c447652f7a67a6cd6bbad25f1b0c7736aa
AnthonyMorales <anthony.morales03@epn.edu.ec>
2025-06-08 14:36:51 -0500

    Agregar Actividad Perfil y su navegación

---
+++ app/src/main/java/com/proano/estefano/lashuequitasapp/PerfilActivity.kt
@@ -0,0 +1,37 @@
+package com.proano.estefano.lashuequitasapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class PerfilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_perfil)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    startActivity(Intent(this, HomeActivity::class.java))
+                    true
+                }
+                else -> false
+            }
+        }
+        val closeProfile = findViewById<ImageView>(R.id.closeProfile)
+        closeProfile.setOnClickListener {
+            finish()
+        }
+    }
+}
