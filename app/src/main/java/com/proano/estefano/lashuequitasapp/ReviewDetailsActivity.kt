b07a6c7c622990b0bd20b6a99075e82cdd69f3d1
Gianfranco Nicolás <gianfrancopr08@gmail.com>
2025-06-10 14:56:09 -0500

    Feat(Gian P): Add logic to ReviewDetailsActivity

--- app/src/main/java/com/proano/estefano/lashuequitasapp/ReviewDetailsActivity.kt
+++ app/src/main/java/com/proano/estefano/lashuequitasapp/ReviewDetailsActivity.kt
@@ -1,6 +1,7 @@
 package com.proano.estefano.lashuequitasapp
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +12,19 @@ class ReviewDetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_review_details)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        // Ajuste de inset para edge-to-edge sobre el root correcto
+        ViewCompat.setOnApplyWindowInsetsListener(
+            findViewById(R.id.detail_root)
+        ) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Cerrar Activity al pulsar la flecha de atrás
+        findViewById<ImageView>(R.id.btnBack).setOnClickListener {
+            finish()
         }
     }
 }
