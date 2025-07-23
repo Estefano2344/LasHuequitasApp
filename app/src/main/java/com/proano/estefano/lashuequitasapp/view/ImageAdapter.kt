6727a05cada874d411726fee1e18ec4d3c760735
AnthonyMorales <anthony.morales03@epn.edu.ec>
2025-07-23 16:14:29 -0500

    Add ImageAdapter for displaying image list

---
+++ app/src/main/java/com/proano/estefano/lashuequitasapp/view/ImageAdapter.kt
@@ -0,0 +1,30 @@
+package com.proano.estefano.lashuequitasapp.view
+
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.proano.estefano.lashuequitasapp.R
+
+
+class ImageAdapter(private val imageList: List<Uri>) :
+    RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
+
+    class ImageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val imageView: ImageView = view.findViewById(R.id.imageItem)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_image, parent, false)
+        return ImageViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        holder.imageView.setImageURI(imageList[position])
+    }
+
+    override fun getItemCount() = imageList.size
+}
