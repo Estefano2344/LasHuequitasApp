00837e48dc7febc5debc8e1780d4984ce976c886
AnthonyMorales <anthony.morales03@epn.edu.ec>
2025-07-23 11:41:03 -0500

    Add user authentication method to UserRepository

--- app/src/main/java/com/proano/estefano/lashuequitasapp/model/businesslogic/UserRepository.kt
+++ app/src/main/java/com/proano/estefano/lashuequitasapp/model/businesslogic/UserRepository.kt
@@ -68,4 +68,44 @@ class UserRepository(context: Context) {
             db.close()
         }
     }
+
+    fun authenticateUser(emailOrUsername: String, password: String): User? {
+        val db = dbHelper.readableDatabase
+        return try {
+            val cursor = db.query(
+                DatabaseHelper.TABLE_USERS,
+                null, // Seleccionar todas las columnas
+                "${DatabaseHelper.COLUMN_EMAIL} = ? OR ${DatabaseHelper.COLUMN_USUARIO} = ?",
+                arrayOf(emailOrUsername, emailOrUsername),
+                null, null, null
+            )
+
+            if (cursor.moveToFirst()) {
+                val storedPassword = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.COLUMN_PASSWORD))
+                if (storedPassword == password) { // En producción, comparar hash de contraseñas
+                    val user = User(
+                        id = cursor.getLong(cursor.getColumnIndexOrThrow(DatabaseHelper.COLUMN_ID)),
+                        nombre = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.COLUMN_NOMBRE)),
+                        apellido = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.COLUMN_APELLIDO)),
+                        email = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.COLUMN_EMAIL)),
+                        password = storedPassword,
+                        usuario = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.COLUMN_USUARIO)),
+                        preferenciasGastronomicas = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.COLUMN_PREFERENCIAS)) ?: ""
+                    )
+                    cursor.close()
+                    user
+                } else {
+                    cursor.close()
+                    null
+                }
+            } else {
+                cursor.close()
+                null
+            }
+        } catch (e: Exception) {
+            null
+        } finally {
+            db.close()
+        }
+    }
 }
