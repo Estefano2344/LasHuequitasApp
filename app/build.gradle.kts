74df17c47dca4e1e62c74d93a355b1a8a4d6ca2f
AnthonyMorales <anthony.morales03@epn.edu.ec>
2025-07-23 23:27:23 -0500

    Add kotlinx.coroutines dependencies to project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,9 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.activity.ktx)
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
