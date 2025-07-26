c908eb0c4ba5d688fa57b52576228959fb8a4abe
Sebastian Guerra <sebastian.guerra01@epn.edu.ec>
2025-07-26 15:15:46 -0500

    Omitir pantallas de inicio y login si el usuario ya está logueado

--- app/src/main/java/com/proano/estefano/lashuequitasapp/view/PantallaInicioActivity.kt
+++ app/src/main/java/com/proano/estefano/lashuequitasapp/view/PantallaInicioActivity.kt
@@ -8,10 +8,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.proano.estefano.lashuequitasapp.R
+import com.proano.estefano.lashuequitasapp.model.businesslogic.SessionManager
 
 class PantallaInicioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val sessionManager = SessionManager(this)
+        if (sessionManager.isLoggedIn()) {
+            val intent = Intent(this, HomeActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            finish()
+            return
+        }
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_pantalla_inicio)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
